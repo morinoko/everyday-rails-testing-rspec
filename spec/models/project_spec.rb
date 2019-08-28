de85c457daafca9b05992279baca2ab635b4926c
morinoko <forby.1@gmail.com>
2019-08-28 14:22:27 +0900

    refactor duplicate project names tests

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,55 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  it "can have many notes" do
+  xit "can have many notes" do
     project = FactoryBot.create(:project, :with_notes)
     expect(project.notes.length).to eq 5
   end
 
-  it "does not allow duplicate project names within a single user" do
-    user = User.create(
-      first_name:  "Joe",
-      last_name:  "Tester",
-      email:      "tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze"
-    )
+  describe "duplicate project names" do
 
-    user.projects.create(
-      name: "Test Project"
-    )
+    before do
+      @user = FactoryBot.create(:user)
+      @project = FactoryBot.create(:project, owner: @user, name: "Rails app")
+    end
 
-    new_project = user.projects.build(
-      name: "Test Project"
-    )
+    context "within a single user" do
+      it "does not allow duplicate project names" do
+        new_project = FactoryBot.build(:project, owner: @user, name: "Rails app")
 
-    new_project.valid?
-    expect(new_project.errors[:name]).to include("has already been taken")
-  end
+        new_project.valid?
+        expect(new_project.errors[:name]).to include("has already been taken")
+      end
+    end
 
-  it "allows two different users to have the same project name" do
-    user = User.create(
-      first_name:  "Joe",
-      last_name:  "Tester",
-      email:      "joe-tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze"
-    )
+    context "across different users" do
+      it "does allow duplicate project names" do
+        another_user = FactoryBot.create(:user)
+        another_users_project = FactoryBot.build(:project, owner: another_user, name: "Rails app")
 
-    user.projects.create(
-      name: "Test Project"
-    )
-
-    other_user = User.create(
-      first_name: "Jane",
-      last_name: "Tester",
-      email: "jane-tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze"
-    )
-
-    other_project = other_user.projects.create(
-      name: "Test Project"
-    )
-
-    expect(other_project).to be_valid
+        expect(another_users_project).to be_valid
+      end
+    end
   end
 
   describe "late status" do
