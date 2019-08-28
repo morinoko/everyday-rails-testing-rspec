f406f68cf3a244ccd609219b44a3dd93a4b66190
morinoko <forby.1@gmail.com>
2019-08-28 12:20:32 +0900

    create user factory and refactor user model tests

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    first_name "Felice"
+    last_name "Forby"
+    email "tester@example.com"
+    password "dottle-nouveau-pavilion-tights-furze"
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it "has a valid factory" do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
+
   it "is valid with a first name, last name, email, and password" do
     user = User.new(
       first_name: "Joe",
@@ -13,50 +17,32 @@ RSpec.describe User, type: :model do
   end
 
   it "is invalid without a first name" do
-    user = User.new(first_name: nil)
+    user = FactoryBot.build(:user, first_name: nil)
     user.valid?
     expect(user.errors[:first_name]).to include("can't be blank")
   end
 
   it "is invalid without a last name" do
-    user = User.new(last_name: nil)
+    user = FactoryBot.build(:user, last_name: nil)
     user.valid?
     expect(user.errors[:last_name]).to include("can't be blank")
   end
 
   it "is invalid without an email address" do
-    user = User.new(email: nil)
+    user = FactoryBot.build(:user, email: nil)
     user.valid?
     expect(user.errors[:email]).to include("can't be blank")
   end
 
   it "is invalid without a duplicate email address" do
-    User.create(
-      first_name:  "Joe",
-      last_name:  "Tester",
-      email:      "tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze"
-    )
-
-    user = User.new(
-      first_name: "Jane",
-      last_name: "Tester",
-      email: "tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze"
-    )
-
-    user.valid?
-    expect(user.errors[:email]).to include("has already been taken")
+    user = FactoryBot.create(:user, email: "joe@example.com")
+    other_user = FactoryBot.build(:user, email: "joe@example.com")
+    other_user.valid?
+    expect(other_user.errors[:email]).to include("has already been taken")
   end
 
   it "returns a user's full name as a string" do
-    user = User.create(
-      first_name: "Joe",
-      last_name: "Tester",
-      email: "tester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze"
-    )
-
+    user = FactoryBot.build(:user, first_name: "Joe", last_name: "Tester")
     expect(user.name).to eq("Joe Tester")
   end
 end
