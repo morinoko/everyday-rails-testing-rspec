9b75317153ff39d818229de679aca89a6594f7b8
morinoko <forby.1@gmail.com>
2019-08-28 15:36:50 +0900

    add test for http response code

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -5,7 +5,11 @@ RSpec.describe HomeController, type: :controller do
     it "responds successfully" do
       get :index
       expect(response).to be_success
-      # or expect(response).to have_http_status "200"
+    end
+
+    it "returns a 200 response" do
+      get :index
+      expect(response).to have_http_status "200"
     end
   end
 end
