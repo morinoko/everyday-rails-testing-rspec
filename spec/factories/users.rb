42002f45514ceace54213362e7a9bf61c862b3de
morinoko <forby.1@gmail.com>
2019-08-28 12:21:53 +0900

    use sequencer for user factory email attribute

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     first_name "Felice"
     last_name "Forby"
-    email "tester@example.com"
+    sequence(:email) { |n| "tester#{n}@example.com" }
     password "dottle-nouveau-pavilion-tights-furze"
   end
 end
