ddcf033a3a304217f4e428a5b96dc5c13150609a
morinoko <forby.1@gmail.com>
2019-08-29 14:21:37 +0900

    refactor note spec to use let

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project, owner: user) }
 
-  # set up test data for ALL test in the file
-  before do
-    @user = FactoryBot.create(:user)
-    @project = FactoryBot.create(:project, owner: @user)
-  end
+  # set up test data for ALL test in the file if not using let
+  # before do
+  #   @user = FactoryBot.create(:user)
+  #   @project = FactoryBot.create(:project, owner: @user)
+  # end
 
   # validation tests
   it "is valid with a user, project, and message" do
     note = Note.new(
       message: "This is a sample note",
-      user: @user,
-      project: @project
+      user: user,
+      project: project
     )
 
     expect(note).to be_valid
@@ -28,33 +30,43 @@ RSpec.describe Note, type: :model do
   # search test
   describe "search message for a term" do
 
-    # set up extra test data for all tests in this describe block
-    before do
-      @note1 = @project.notes.create(
-        message: "This is the first note.",
-        user: @user
+    # need to use let! or else all notes won't be created
+    # may be better off using a before block...
+    let!(:note1) { 
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
+        message: "This is the first note."
       )
+    }
 
-      @note2 = @project.notes.create(
-        message: "This is the second note.",
-        user: @user
+    let!(:note2) { 
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
+        message: "This is the second note."
       )
+    }
 
-      @note3 = @project.notes.create(
-        message: "First, preheat the oven.",
-        user: @user
+    let!(:note3) { 
+      FactoryBot.create(:note,
+        project: project,
+        user: user,
+        message: "First, preheat the oven."
       )
-    end
+    }
 
     context "when a match is found" do
       it "returns notes that match the search term" do
-        expect(Note.search("first")).to include(@note1, @note3)
+        expect(Note.search("first")).to include(note1, note3)
+        expect(Note.count).to eq 3
       end
     end
 
     context "when no matches are found" do
       it "returns an empty collection" do
         expect(Note.search("message")).to be_empty
+        expect(Note.count).to eq 3
       end
     end
   end
