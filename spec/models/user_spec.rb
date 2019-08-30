5bdb10d81e5c6776a0dcc22589c01edb738c2f87
morinoko <forby.1@gmail.com>
2019-08-30 11:15:23 +0900

    test that geocoding actually works

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -67,4 +67,13 @@ RSpec.describe User, type: :model do
     # for mailer to be sent
     expect(UserMailer).to have_received(:welcome_email).with(user)
   end
+
+  it "performs geocoding" do
+    user = FactoryBot.create(:user, last_sign_in_ip: "161.185.207.20")
+    expect {
+      user.geocode
+    }.to change(user, :location).
+      from(nil).
+      to("Brooklyn, New York, US")
+  end
 end
